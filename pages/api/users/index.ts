a85786e7ffaefe8d33380781f729436aad7c01b9
Jose Contreras <jocontrera16@gmail.com>
2022-09-18 20:52:42 -0300

    added: changing order by post model

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -22,7 +22,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         return res.status(400).json({ error: "Invalid post data" });
       }
 
-      const createdPost = await prisma.order.create({
+      const createdPost = await prisma.post.create({
         data: {
           ...post,
         },
